6b8e5a312ab34a3c176bb0043630eea974c21e44
Anas <amanabbasi3660@gmail.com>
2022-06-10 16:10:49 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
