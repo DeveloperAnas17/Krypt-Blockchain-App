346984ad09017e776e3430a42383dde738a27dc0
Anas <amanabbasi3660@gmail.com>
2022-06-14 10:53:22 +0530

    add welcome page

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,5 +1,9 @@
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className=" flex justify-center items-center p-3 ">
+      <div className="animate-spin rounded-full w-28 h-28 border-b-2 border-red-700" />
+    </div>
+  );
 };
 
 export default Loader;
